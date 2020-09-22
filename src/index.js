dedf95cbec0bedbd10813f889f569dade264c4ff
Vagner Treguas <vagner.treguas@gmail.com>
2020-09-22 15:37:09 -0300

    Components Based in Class

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App'
+import App from './components/App';
+import App2 from './components/App2'
 
 
 ReactDOM.render(
-    <App nome="Vagner" sobrenome="Treguas" home="ao React"/>,
+    <div>
+    <App nome="Vagner" sobrenome="Treguas" home="ao React"/>
+    <App2 /></div>,
     document.getElementById('root')
 )
